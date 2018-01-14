af68eb8b8b1a8382db5eb0bb557afff3d000bfac
FANTOME000 <34143220+FANTOME000@users.noreply.github.com>
2018-01-14 03:18:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get("/", function(req, res) {
-    res.send("selftbot lancer, https://blog.artstorefronts.com/wp-content/uploads/2016/03/ASF-Artists-Website-09.gif");
+    res.send("selftbot lancer" https://blog.artstorefronts.com/wp-content/uploads/2016/03/ASF-Artists-Website-09.gif);
 });
 
 app.listen(port);
