020d7571315b00b274a9e1e9133d51799de8c9bb
FANTOME000 <34143220+FANTOME000@users.noreply.github.com>
2018-01-14 20:05:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,29 @@ client.on('message', msg => {
     if (!msg.guild && msg.author.id !== "317375697700126720"){ msg.reply("Je te réponds quand je peut (message automatique)") msg.delete(1000);}
   if (msg.content.startsWith("<@317375697700126720>")) {
     msg.reply('Je te réponds quand je peut (message automatique)');
-  }
+  }else if (msg.content.startsWith("#kikoo")) {
+    if (msg.content.replace("#kikoo ", "") === "") {
+      msg.reply("`kikoo <Query>`");
+    }
+    request(
+      "https://www.googleapis.com/youtube/v3/search?part=snippet&order=viewCount&q=" +
+        msg.content.replace("#kikoo ", "") +
+        "&type=video&videoDefinition=high&key=AIzaSyDs1NJ5wF-pvykTHh_9_CP4tIrkQzuFRqw",
+      (error, response, body) => {
+        if (error || response.statusCode !== 200) {
+          msg.reply("L'API de Google ne fonctionne pas lol.");
+        } else {
+          body = JSON.parse(body);
+          if (body.pageInfo.totalResults === 0) {
+            msg.reply("Sans résultats lol."); // nice french
+          } else {
+            msg.reply(
+              "La première vidéo: http://youtu.be/" + body.items[0].id.videoId
+            );
+          }
+        }
+      }
+    );
    });
 
 client.login("MzE3Mzc1Njk3NzAwMTI2NzIw.DRsYJw.VW7FV1-uymvMdTIuErGvSsLcrSA");
