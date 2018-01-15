ba81cede6f5093ceff3e5a9606dd9f9864ccc7d5
FANTOME000 <34143220+FANTOME000@users.noreply.github.com>
2018-01-15 03:43:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const Ffmpeg = require("Ffmpeg.js");
+const Ffmpeg = require("ffmpeg.js");
 const ffmpeg = require("ffmpeg");
 const client = new Discord.Client();
 var express = require("express");
